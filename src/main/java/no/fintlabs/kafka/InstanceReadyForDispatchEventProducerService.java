49acc3301e7dce389477a3874dffde7037ff856b
Egil Ballestad <egil.ballestad@vigoiks.no>
2025-05-22 12:12:34 +0200

    add retention time in ms property for instance topics

--- src/main/java/no/fintlabs/kafka/InstanceReadyForDispatchEventProducerService.java
+++ src/main/java/no/fintlabs/kafka/InstanceReadyForDispatchEventProducerService.java
@@ -6,6 +6,7 @@ import no.fintlabs.flyt.kafka.event.InstanceFlowEventProducerRecord;
 import no.fintlabs.flyt.kafka.headers.InstanceFlowHeaders;
 import no.fintlabs.kafka.event.topic.EventTopicNameParameters;
 import no.fintlabs.kafka.event.topic.EventTopicService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,14 +17,15 @@ public class InstanceReadyForDispatchEventProducerService {
 
     public InstanceReadyForDispatchEventProducerService(
             InstanceFlowEventProducerFactory eventProducerFactory,
-            EventTopicService eventTopicService
+            EventTopicService eventTopicService,
+            @Value("${fint.kafka.topic.instance-retention-ms}") long retentionMs
     ) {
         this.eventProducer = eventProducerFactory.createProducer(Object.class);
         eventTopicNameParameters = EventTopicNameParameters
                 .builder()
                 .eventName("instance-ready-for-dispatch")
                 .build();
-        eventTopicService.ensureTopic(eventTopicNameParameters, 0);
+        eventTopicService.ensureTopic(eventTopicNameParameters, retentionMs);
     }
 
     public void publish(InstanceFlowHeaders instanceFlowHeaders, Object instance) {
